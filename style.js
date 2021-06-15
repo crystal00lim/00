b11e3b93a7617dbaa0786eb1cc9e3200ea2cd529
crystal00lim <71657775+crystal00lim@users.noreply.github.com>
2021-06-15 15:48:59 +0000

    debugged

--- style.js
+++ style.js
@@ -1,11 +1,11 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "350px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("mySidenav").style.width = "290px";
+  document.getElementById("mySidenav").style.marginLeft = "0px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("mySidenav").style.marginLeft= "0";
   document.body.style.backgroundColor = "white";
 }
